c8aacfe57eb7abed57ef4de30f65272159b406ef
RDCarleton <xevoker007@gmail.com>
2021-02-11 23:22:31 -0500

    First

--- src/main/java/AddressBook/BuddyInfoRepository.java
+++ src/main/java/AddressBook/BuddyInfoRepository.java
@@ -1,7 +1,7 @@
 package AddressBook;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
+//comment
 @RepositoryRestResource(collectionResourceRel = "buddyInfo", path = "buddyInfo")
 public interface BuddyInfoRepository extends CrudRepository<BuddyInfo, String>{
     BuddyInfo findByName(String name);
